2dad28b1de52295ef4edda357427d62e2589f015
Christoph <christoph.joerimann@stud.hslu.ch>
2016-03-04 11:17:57 +0100

    Dev CJ

---
+++ PREN/hslu/pren/DEV/DevelopmentCJ.py
@@ -0,0 +1,10 @@
+
+from hslu.pren.visuals import ContainerDetection
+
+if __name__ == '__main__':
+    
+    detector = ContainerDetection.ContainerDetector()
+    
+    ret = detector.CheckContainer()
+    
+    pass
---
+++ PREN/hslu/pren/navigation/Navigator.py
@@ -0,0 +1,17 @@
+'''
+Created on 04.03.2016
+
+@author: Christoph
+@todo: Methoden implementieren
+'''
+
+import hslu.pren.common
+
+
+class Navigator():
+    
+    #Constructor
+    def __init__(self, webcamPort):
+        self.port = webcamPort
+        
+    def GetCorrectionAngle(self):
