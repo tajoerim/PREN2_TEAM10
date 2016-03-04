162d4622ecc1b220b8e501dd2ccac9cbb4fd6cc6
Christoph <christoph.joerimann@stud.hslu.ch>
2016-03-04 11:17:11 +0100

    CJ Dev

--- PREN/hslu/pren/visuals/ContainerDetection.py
+++ PREN/hslu/pren/visuals/ContainerDetection.py
@@ -6,6 +6,10 @@ Created on 08.12.2015
 @todo: Implementiation
 '''
 
+import cv2
+import numpy as np
+from imutils import contours
+
 class ContainerDetector():
 
     def __init__(self, color="blue"):
@@ -17,12 +21,73 @@ class ContainerDetector():
         mit der gesuchten Farbe handelt
         '''
         
+        isColor = self.CheckColor()
+        if (isColor == True):
+            return self.CheckContour()
+        
         raise NotImplementedError( "Should have implemented this" )    
     
     def CheckColor(self):
         '''
         Prueft ob der Container der richtigen Farbe entspricht
         '''
+        
+        width = 320
+        height = 240
+        
+        rangeX1 = 0
+        rangeX2 = width
+        rangeY1 = 20
+        rangeY2 = 200
+        
+        minFlaeche = 15000
+            
+        # initialize the camera and grab a reference to the raw camera capture
+        cap = cv2.VideoCapture(0)
+        
+        while(True):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+        
+            # Our operations on the frame come here
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            
+            both = self.Getthresholdedimg(hsv)
+            erode = cv2.erode(both,None,iterations = 3)
+            dilate = cv2.dilate(erode,None,iterations = 10)
+        
+            contours,hierarchy = cv2.findContours(dilate,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+            
+            maxX = 0
+            maxY = 0
+            maxW = 0
+            maxH = 0
+            
+            for cnt in contours:
+                x,y,w,h = cv2.boundingRect(cnt)
+                
+                if (y > rangeY1 and y < rangeY2 and x > rangeX1 and x < rangeX2):
+                    if (w*h > minFlaeche and w*h > maxW*maxH):
+                        maxX = x
+                        maxY = y
+                        maxH = h
+                        maxW = w
+            
+            cv2.rectangle(frame, (maxX,maxY), (maxX+maxW,maxY+maxH), (0,255,0), thickness=3, lineType=8, shift=0)
+            cv2.putText(frame, str(maxW*maxH), (maxX,maxY), cv2.FONT_HERSHEY_SIMPLEX, 4,(255,255,255),2)
+            
+            cv2.drawContours(frame, contours, -1, (255,0,0), 1)
+        
+            cv2.rectangle(frame, (rangeX1,rangeY1), (rangeX2,rangeY2), (255,0,0), thickness=1, lineType=8, shift=0)
+        
+            # Display the resulting frame
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        
+        # When everything done, release the capture
+        cap.release()
+        cv2.destroyAllWindows()
         
         raise NotImplementedError( "Should have implemented this" )
     
@@ -38,7 +103,7 @@ class ContainerDetector():
         Prueft ob sich der Container innerhalb des Greifers befindet
         
         Kleiner 0:    Zu weit links
-        Grösser 0:    Zu weit rechts
+        Groesser 0:    Zu weit rechts
         Gleich 0:    Mittig
         '''
         
@@ -49,9 +114,16 @@ class ContainerDetector():
         Prueft ob sich der Container innerhalb des Greifers befindet
         
         Kleiner 0:         Zu weit weg
-        Grösser gleich 0:  Positioniert
+        Groesser gleich 0:  Positioniert
         '''
         
         raise NotImplementedError( "Should have implemented this" )
+    
+    
+    def Getthresholdedimg(self, hsv):
+        yellow = cv2.inRange(hsv,np.array((0,115,84)),np.array((217,255,171)))
+        blue = cv2.inRange(hsv,np.array((100,0,100)),np.array((120,255,255)))
+        both = cv2.add(yellow,blue)
+        return both
     
         
